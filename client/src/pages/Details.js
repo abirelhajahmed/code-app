a03d7ec5bbbe05c644b0abfed1d9dea031bef6ac
root <root@DESKTOP-3JLJ9O9>
2023-07-23 11:20:28 -0700

    commit changes

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,13 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import InputGroup from '../components/InputGroup'
+import axios from 'axios';
+import InputGroup from '../components/InputGroup';
 
 function Details() {
- 
   const [form, setForm] = useState({});
-  const {id} = useParams();
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [errors, setErrors] = useState({});
 
   const onChangeHandler = (e) => {
@@ -15,71 +14,78 @@ function Details() {
       ...form,
       [e.target.name]: e.target.value,
     });
-    
+    // Clear the error message when the user starts typing again
+    setErrors({
+      ...errors,
+      [e.target.name]: '',
+    });
   };
 
-  const onSubmitHandler = (e)=>{
+  const onSubmitHandler = (e) => {
     e.preventDefault();
-    axios.put(`/api/users/${id}`, form)
-    .then(res=>{
-      navigate('/')
-    })
-    .catch(err=>setErrors(err.response.data))
-    
-  }
+    axios
+      .post(`/api/users`, form) // Use POST method to add a new user
+      .then((res) => {
+        navigate('/');
+      })
+      .catch((err) => setErrors(err.response.data));
+  };
 
   useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`/api/users/${id}`);
-      setForm(response.data);
-    } catch (error) {
-      // Handle any error that occurred during the request
-    }
-  };
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/users/${id}`);
+        setForm(response.data);
+      } catch (error) {
+        // Handle any error that occurred during the request
+        // For example, you might want to show an error message or navigate back to the home page
+      }
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, [id]);
 
   return (
     <div className="container mt-4 col-12 col-lg-4">
-        <form onSubmit={onSubmitHandler}>
-          <InputGroup
-            label="Email"
-            type="text"
-            name="Email"
-            onChangeHandler={onChangeHandler}
-            errors={errors.Email}
-            value={form.Email}
-          />
-          <InputGroup
-            label="Lastname"
-            type="text"
-            name="Lastname"
-            onChangeHandler={onChangeHandler}
-            errors={errors.Lastname}
-            value={form.Lastname}
-          />
-          <InputGroup
-            label="Firstname"
-            type="text"
-            name="Firstname"
-            onChangeHandler={onChangeHandler}
-            errors={errors.Firstname}
-            value={form.Firstname}
-          />
-          <InputGroup
-            label="Age"
-            type="text"
-            name="Age"
-            onChangeHandler={onChangeHandler}
-            errors={errors.Age}
-            value={form.Age}
-          />
-          <button className="btn btn-primary" type="submit">Add user</button>
-        </form>
-      </div>
-  )
+      <form onSubmit={onSubmitHandler}>
+        <InputGroup
+          label="Email"
+          type="text"
+          name="email"
+          onChangeHandler={onChangeHandler}
+          errors={errors.email}
+          value={form.email || ''}
+        />
+        <InputGroup
+          label="Lastname"
+          type="text"
+          name="lastname"
+          onChangeHandler={onChangeHandler}
+          errors={errors.lastname}
+          value={form.lastname || ''}
+        />
+        <InputGroup
+          label="Firstname"
+          type="text"
+          name="firstname"
+          onChangeHandler={onChangeHandler}
+          errors={errors.firstname}
+          value={form.firstname || ''}
+        />
+        <InputGroup
+          label="Age"
+          type="text"
+          name="age"
+          onChangeHandler={onChangeHandler}
+          errors={errors.age}
+          value={form.age || ''}
+        />
+        <button className="btn btn-primary" type="submit">
+          Add user
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Details
+export default Details;
