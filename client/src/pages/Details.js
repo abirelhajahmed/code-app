f7e156fdc7578e0f1a853aef442bcbb09351c9e8
root <root@DESKTOP-3JLJ9O9>
2023-07-25 16:33:57 -0700

    commit changes

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,13 +1,13 @@
-// src/pages/Details.js
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
-import InputGroup from '../components/InputGroup';
+import InputGroup from '../components/InputGroup'
 
 function Details() {
+ 
   const [form, setForm] = useState({});
-  const { id } = useParams();
-  const navigate = useNavigate();
+  const {id} = useParams();
+  const navigate = useNavigate()
   const [errors, setErrors] = useState({});
 
   const onChangeHandler = (e) => {
@@ -15,72 +15,71 @@ function Details() {
       ...form,
       [e.target.name]: e.target.value,
     });
+    
   };
 
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler = (e)=>{
     e.preventDefault();
-    axios
-      .put(`/api/users/${id}`, form)
-      .then((res) => {
-        navigate('/');
-      })
-      .catch((err) => setErrors(err.response.data));
-  };
+    axios.put(`/api/users/${id}`, form)
+    .then(res=>{
+      navigate('/')
+    })
+    .catch(err=>setErrors(err.response.data))
+    
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`/api/users/${id}`);
-        setForm(response.data);
-      } catch (error) {
-        // Handle any error that occurred during the request
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`/api/users/${id}`);
+      setForm(response.data);
+    } catch (error) {
+      // Handle any error that occurred during the request
+    }
+  };
 
-    fetchData();
-  }, [id]); // Include 'id' in the dependency array
+  fetchData();
+}, []);
 
   return (
     <div className="container mt-4 col-12 col-lg-4">
-      <form onSubmit={onSubmitHandler}>
-        <InputGroup
-          label="Email"
-          type="text"
-          name="Email"
-          onChangeHandler={onChangeHandler}
-          errors={errors.Email}
-          value={form.Email || ''}
-        />
-        <InputGroup
-          label="Lastname"
-          type="text"
-          name="Lastname"
-          onChangeHandler={onChangeHandler}
-          errors={errors.Lastname}
-          value={form.Lastname || ''}
-        />
-        <InputGroup
-          label="Firstname"
-          type="text"
-          name="Firstname"
-          onChangeHandler={onChangeHandler}
-          errors={errors.Firstname}
-          value={form.Firstname || ''}
-        />
-        <InputGroup
-          label="Age"
-          type="text"
-          name="Age"
-          onChangeHandler={onChangeHandler}
-          errors={errors.Age}
-          value={form.Age || ''}
-        />
-        <button className="btn btn-primary" type="submit">
-          Add user
-        </button>
-      </form>
-    </div>
-  );
+        <form onSubmit={onSubmitHandler}>
+          <InputGroup
+            label="Email"
+            type="text"
+            name="Email"
+            onChangeHandler={onChangeHandler}
+            errors={errors.Email}
+            value={form.Email}
+          />
+          <InputGroup
+            label="Lastname"
+            type="text"
+            name="Lastname"
+            onChangeHandler={onChangeHandler}
+            errors={errors.Lastname}
+            value={form.Lastname}
+          />
+          <InputGroup
+            label="Firstname"
+            type="text"
+            name="Firstname"
+            onChangeHandler={onChangeHandler}
+            errors={errors.Firstname}
+            value={form.Firstname}
+          />
+          <InputGroup
+            label="Age"
+            type="text"
+            name="Age"
+            onChangeHandler={onChangeHandler}
+            errors={errors.Age}
+            value={form.Age}
+          />
+          <button className="btn btn-primary" type="submit">Add user</button>
+        </form>
+      </div>
+  )
 }
 
-export default Details;
+export default Details
